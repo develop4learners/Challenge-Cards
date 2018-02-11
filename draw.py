4a597fce7a26bc5ba1dc7fc5aa594522ba90393a
devjw2 <mcjeffrey168@gmail.com>
2018-02-11 16:03:59 -0500

    done

--- draw.py
+++ draw.py
@@ -1,4 +1,61 @@
 from display import *
 
+
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+
+    #swap
+    if x1 < x0: 
+        draw_line(x1, y1, x0, y0, screen, color)
+
+
+    x = x0
+    y = y0 
+    a = y1 - y0
+    b = x0 - x1
+
+    #slope 
+    if (x1 - x0) != 0:
+        m = float(a)/float(-1 * b)
+    else: 
+        m = 0
+
+    #OCTANT 1
+    if m >= 0 and m <= 1: 
+        d = 2 * a + b
+        while x <= x1:
+            plot(screen, color, x, y)
+            if d > 0:
+                y += 1
+                d += 2 * b
+            x += 1
+            d += 2 * a
+    #OCTANT 2
+    elif m > 1: 
+        d = a + 2 * b
+        while y <= y1:
+            plot(screen,color, x, y)
+            if d < 0:
+                x += 1
+                d += 2 * a
+            y += 1
+            d += 2 * b
+    #OCTANT 7
+    elif m < -1: 
+        d = a - 2 * b
+        while y >= y1:
+            plot(screen, color, x, y)
+            if d > 0: 
+                x += 1
+                d += 2 * a
+            y -= 1
+            d -= 2 * b
+    #OCTANT 8
+    elif m >= -1:
+        d = 2 * a - b
+        while x <= x1:
+            plot(screen, color, x, y)
+            if d < 0: 
+                y -= 1
+                d -= 2 * b
+            x += 1
+            d += 2 * a
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from display import *
 from draw import *
+from math import *
+import random
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+#TEST
+
+for i in range(36):
+    i += 10
+    color[0] = random.randint(0,255)
+    color[1] = random.randint(0,255)
+    color[2] = random.randint(0,255)
+    draw_line(XRES/2, YRES/2, XRES/2 + int(cos(i) * 100), YRES/2 + int(sin(i) * 100), screen, color)
+
+
+
 display(screen)
+
 save_extension(screen, 'img.png')
