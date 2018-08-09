dc2544d82794558312c8fa03f42e0013ac40b6b0
develop4learners <cqr2@cornell.edu>
2018-08-09 19:29:43 -0400

    Change drawLine for the hardware for GrowThings

--- growThings_drawLine.py
+++ growThings_drawLine.py
@@ -1,59 +1,55 @@
-from display import *
-
-
-def draw_line( x0, y0, x1, y1, screen, color ):
+def drawLine(x0, y0, x1, y1):
 
     #swap
-    if x1 < x0: 
-        draw_line(x1, y1, x0, y0, screen, color)
-
+    if x1 < x0:
+        drawLine(x1, y1, x0, y0)
 
     x = x0
-    y = y0 
+    y = y0
     a = y1 - y0
     b = x0 - x1
 
     if (b == 0):
         if (y < y1):
             while(y <= y1):
-                plot(screen, color, x, y)
+                oled.pixel(x, y, 1)
                 y = y + 1
         if (y > y1):
             while(y1 <= y):
-                plot(screen, color, x, y)
+                oled.pixel(x, y, 1)
                 y = y - 1
         return
 
-    #slope 
+    #slope
     m = float(a)/float(-1 * b)
- 
+
 
     #OCTANT 1
-    if m >= 0 and m <= 1: 
+    if m >= 0 and m <= 1:
         d = 2 * a + b
         while x <= x1:
-            plot(screen, color, x, y)
+            oled.pixel(x, y, 1)
             if d > 0:
                 y += 1
                 d += 2 * b
             x += 1
             d += 2 * a
     #OCTANT 2
-    elif m > 1: 
+    elif m > 1:
         d = a + 2 * b
         while y <= y1:
-            plot(screen,color, x, y)
+            oled.pixel(x, y, 1)
             if d < 0:
                 x += 1
                 d += 2 * a
             y += 1
             d += 2 * b
     #OCTANT 7
-    elif m < -1: 
+    elif m < -1:
         d = a - 2 * b
         while y >= y1:
-            plot(screen, color, x, y)
-            if d > 0: 
+            oled.pixel(x, y, 1)
+            if d > 0:
                 x += 1
                 d += 2 * a
             y -= 1
@@ -62,9 +58,12 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     elif m >= -1:
         d = 2 * a - b
         while x <= x1:
-            plot(screen, color, x, y)
-            if d < 0: 
+            oled.pixel(x, y, 1)
+            if d < 0:
                 y -= 1
                 d -= 2 * b
             x += 1
             d += 2 * a
+    oled.show()
+
+drawLine(3, 100, 100, 10)
